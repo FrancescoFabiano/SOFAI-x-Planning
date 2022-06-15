afa307e521f00427c58ef82e7fbf07b982d8c4c9
FrancescoFabiano <francesco.fabi94@gmail.com>
2022-06-15 18:32:48 +0200

    Added firs batch of results

--- Output/sol_reader.py
+++ Output/sol_reader.py
@@ -22,11 +22,36 @@ if __name__ == '__main__':
     filename = sys.argv[1]
 
     modfilename = os.path.splitext(filename)[0]+'.csv'  # /home/user/somefile.jpg
+    newfilename = os.path.splitext(filename)[0]+'.txt'  # /home/user/somefile.jpg
+
     #print("ModName is " + modfilename + " and number is " +  str(instance_number))
+    printed=set()
+    okLine = True
+    with open(newfilename, 'w') as f:
+        with open(filename) as myfile:
+            for line in myfile:
+                okLine = True
+                found = 0
+                problem = getVarFromLine(line,"pro")
+                if (problem != ''):
+                    if "could not be solved" in line:
+                        with open(filename) as myfileCheck:
+                            for lineCheck in myfileCheck:
+                                problemCheck = getVarFromLine(lineCheck,"pro")
+                                if problem == problemCheck:
+                                    if "could not be solved" not in lineCheck:
+                                        okLine = False
+                                    else:
+                                        found +=1
+                if (okLine and problem not in printed):
+                    printed.add(problem)
+                    print(line,file=f)
+                            #sys.exit()
+            myfile.close()
 
     with open(modfilename, 'w') as f:
         print("Problem,Time,Correctness,System",file=f)
-        with open(filename) as myfile:
+        with open(newfilename) as myfile:
             for line in myfile:
                 problem = getVarFromLine(line,"pro")
                 if (problem != ''):
@@ -34,7 +59,7 @@ if __name__ == '__main__':
                         print(problem + ", -1,-1,-1",file=f)
                     else:
                         print(problem + ", "+ getVarFromLine(line,"tim")[:-1]+ "," + getVarFromLine(line,"cor")+ "," +getVarFromLine(line,"sys"),file=f)
-                    #sys.exit()
+                        #sys.exit()
             myfile.close()
 
 
