ad8cf69946e0336bc024b6c3041c8eeff891cfaa
FrancescoFabiano <fabianofrancesco.cs@gmail.com>
2023-03-20 22:24:19 -0400

    Pushed local version of FastDonward to preserve local changes

---
+++ Planners/FastDownward/src/search/pruning/null_pruning_method.cc
@@ -0,0 +1,34 @@
+#include "null_pruning_method.h"
+
+#include "../option_parser.h"
+#include "../plugin.h"
+
+using namespace std;
+
+namespace null_pruning_method {
+NullPruningMethod::NullPruningMethod(const Options &opts)
+    : PruningMethod(opts) {
+}
+
+void NullPruningMethod::initialize(const shared_ptr<AbstractTask> &task) {
+    PruningMethod::initialize(task);
+    log << "pruning method: none" << endl;
+}
+
+static shared_ptr<PruningMethod> _parse(OptionParser &parser) {
+    parser.document_synopsis(
+        "No pruning",
+        "This is a skeleton method that does not perform any pruning, i.e., "
+        "all applicable operators are applied in all expanded states. ");
+    add_pruning_options_to_parser(parser);
+
+    Options opts = parser.parse();
+    if (parser.dry_run()) {
+        return nullptr;
+    }
+
+    return make_shared<NullPruningMethod>(opts);
+}
+
+static Plugin<PruningMethod> _plugin("null", _parse);
+}
