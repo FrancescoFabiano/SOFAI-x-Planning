c9d96ec2557dfa8a1a71d9fa98a7a740f6f07f4f
Vishal Pallagani <vishal.pallagani@gmail.com>
2023-03-26 10:06:51 -0400

    corrected subgoal completeness

--- Planners/PDDL_parser/SubgoalCompleteness.py
+++ Planners/PDDL_parser/SubgoalCompleteness.py
@@ -1,38 +1,85 @@
+def convert_string_to_list(input_string):
+    conditions = input_string.split("(and")[-1]
+    conditions = conditions.split(') (')
+    conditions = [c.replace('(not ', 'not ') if c.startswith('(not ') else c for c in conditions]
+    conditions = [c[:-1] if c.endswith(')') else c for c in conditions]
+    return [i.replace("(","").replace(")", "").strip() for i in conditions]
+
+def remove_empty(list_inp):
+    return [i for i in list_inp if len(i)!=0]
+
+import re
+
 def read_domain(path):
     file1 = open(path, 'r')
     lines = file1.readlines()
     dict_act = dict()
     for i in range(len(lines)):
+        if re.match(r"\s*\(:action", lines[i]):
+            act = lines[i].split(' ')[-1].replace("\n", "")
+            dict_act[act] = [0, [],[]]
 
-        if lines[i].startswith('  (:action'):
-            act = lines[i].split(' ')[3].replace("\n", "")
-            dict_act[act] = [0,[]]
-        elif lines[i].startswith("	     :parameters"):
-            params = len(lines[i].split("(")[1].split(" "))
-            dict_act[act][0] = params
-        elif lines[i].startswith('	     :effect'):
+        elif re.match(r"\s*:parameters", lines[i]):
+            all_text = lines[i]
             for j in range(i+1, len(lines)):
-                if lines[j].startswith('	     ('):
-                    dict_act[act][1].append(" ".join(lines[j].split(' ')[6:]).replace("(","").replace(")","").replace("\n", ""))
-                elif lines[j].startswith('		   ('):
-                    dict_act[act][1].append(" ".join(lines[j].split(' ')[3:]).replace("(","").replace(")","").replace("\n", ""))
-                elif lines[j].startswith('  (:action'):
+                if re.match(r"\s*:precondition", lines[j]):
                     break
+                else:
+                    all_text = all_text + lines[j]
+                    continue
+            # print(all_text)
+            all_text = all_text.strip()
+            if '\n' in all_text:
+                # If input_string contains parameter names in parentheses
+                parameter_list = []
+                for line in all_text.strip().split('\n'):
+                    # print(line)
+                    # raise KeyboardInterrupt
+                    if 'parameters' not in line:
+                        parameter_list.append(line.strip().split()[0])
+            else:
+                # If input_string contains parameter names as separate lines
+                parameters = all_text.split('(', 1)[1].split(')')[0].strip()
+                # print(parameters)
+                # raise KeyboardInterrupt
+                if parameters == '':
+                    parameter_list = []
+                else:
+                    parameter_list = parameters.split()
+            parameter_list = [i.replace("(","") for i in parameter_list if i.count("?")]
+            # print(parameter_list)
+            # raise KeyboardInterrupt
+            dict_act[act][0] = all_text.count("?")
+            dict_act[act][1] = dict_act[act][1] + parameter_list
+
+        elif re.match(r"\s*:effect", lines[i]):
+            temp_list = list()
+            flag_update = False
+            for j in range(i, len(lines)):
+                if lines[j].count("effect:"):
+                    if lines[j].count("("):
+                        temp_list = temp_list + convert_string_to_list(lines[j].split("effect:")[1])
+                    else:
+                        continue
+                elif re.match(r"\s*\(:action", lines[j]):
+                    dict_act[act][2] = dict_act[act][2] + remove_empty(temp_list)
+                    flag_update = True
+                    break
+                else:
+                    temp_list = temp_list + convert_string_to_list(lines[j])
+            if flag_update==False:
+                dict_act[act][2] = dict_act[act][2] + remove_empty(temp_list)
         else:
             continue
 
     new_dict = dict()
     for i in dict_act.keys():
         if str(dict_act[i][0]) not in new_dict.keys():
-            new_dict[str(dict_act[i][0])] = dict()
-            new_dict[str(dict_act[i][0])][i] = dict_act[i][1]
+            new_dict[str(dict_act[i][0]).lower()] = dict()
+            new_dict[str(dict_act[i][0]).lower()][i.lower()] = [dict_act[i][1], dict_act[i][2]]
         else:
-            new_dict[str(dict_act[i][0])][i] = dict_act[i][1]
-
-    print("Domain is: " + str(new_dict))
+            new_dict[str(dict_act[i][0]).lower()][i.lower()] = [dict_act[i][1], dict_act[i][2]]
     return new_dict
-
-
 
 def read_plan(plan_text):
     plan = plan_text.split(", ")
@@ -59,9 +106,11 @@ def read_init(path):
     lines = file1.readlines()
     init_cond = list()
     for i in range(len(lines)):
-        if lines[i].startswith('(:init'):
+        # if lines[i].startswith('(:init'):
+        if re.match(r"\s*\(:init", lines[i]):
             for j in range(i+1, len(lines)):
-                if lines[j].startswith('(:goal'):
+                # if lines[j].startswith('(:goal'):
+                if re.match(r"\s*\(:goal", lines[j]):
                     break
                 if lines[j].startswith('(') and lines[j].count("and")==0:
                     temp = lines[j].replace("(","").replace(")","").replace("\n", "")
@@ -88,21 +137,30 @@ def read_goal(path):
     lines = file1.readlines()
     goal = list()
     for i in range(len(lines)):
-        if lines[i].startswith('(:goal'):
+        # if lines[i].startswith('(:goal'):
+        if re.match(r"\s*\(:goal", lines[i]):
+            # print("I am here, read goal ")
+            flag_end = False
             for j in range(i+1, len(lines)):
                 if lines[j].startswith('(') and lines[j].count("and")==0:
                     temp = lines[j].replace("(","").replace(")","").replace("\n", "")
                     if temp.count("not")==0:
-                        temp = temp.split(" ")
+                        temp = temp.strip().split(" ")
+                        # print(temp, len(temp))
                         goal.append([len(temp)-1] + temp)
                     else:
                         temp = temp.split(" ")
+                        # print(temp, len(temp))
                         goal.append([len(temp)-2] + [" ".join(temp[:2])] + temp[2:])
-                elif lines[j].startswith(' '):
+                if lines[j].count(")"):
+                    flag_end = True
+                elif lines[j].startswith(' ') and flag_end==True:
+                    # print("I am here")
+                    # print(j, lines[j])
                     break
         else:
             continue
-
+    # print(goal)
     new_goal = list()
     for i in goal:
         if "not" in " ".join([str(j) for j in i]):
@@ -129,41 +187,47 @@ def check_not(final_state, temp):
 
 def subgoal_completeness(goal, plan, domain, init_cond):
     final_state = init_cond.copy()
+    # print(init_cond)
     for i in plan:
-        if i[0]=='2':
-            temp = domain[i[0]][i[1]]
-            x = i[2].split(" ")[1]
-            y = i[2].split(" ")[2]
-            temp = [j.replace("?x",x).replace("?y",y) for j in temp]
-            if len(final_state)!=0:
-                final_state = check_not(final_state, temp)
-                #final_state = check(final_state, temp)
-            for j in temp:
-                final_state.append(j)
+        temp = domain[i[0]][i[1]]
+        parameter = list()
+        for j in range(int(i[0])):
+            parameter.append(i[2].split(" ")[j+1])
+        changes  = temp[0]
+        temp = temp[1]
+        # print(changes)
+        # print(parameter)
+        # print(temp)
+        # raise KeyboardInterrupt
+        if len(changes)==0:
+            print("No changes required")
         else:
-            print("Domain: " + str(domain))
-            temp = domain[i[0]][i[1]]
-            x = i[2].split(" ")[1]
-            temp = [j.replace("?x",x) for j in temp]
-            if len(final_state)!=0:
-                final_state = check_not(final_state, temp)
-                #final_state = check(final_state, temp)
-            for j in temp:
-                final_state.append(j)
+            update_temp = list()
+            for sub_text in temp:
+                tmp_txt = sub_text + " "
+                for m,n in zip(changes, parameter):
+                    # print(tmp_txt)
+                    # if tmp_txt.count(m+" "):
+                    tmp_txt = tmp_txt.replace(m+" ",n+" ")
+                    # else:
+                        # tmp_txt = tmp_txt.replace(m,n)
+                update_temp.append(tmp_txt.strip())
+            temp = update_temp
+        # print(update_temp)
+        if len(final_state)!=0:
+            final_state = check_not(final_state, temp)
+        for g in temp:
+            final_state.append(g)
     count = 0
     for i in goal:
         if i[2] in final_state:
             count += 1
         else:
             continue
-
     return count/len(goal)
 
-
 def get_correctness(domain_path, plan_text, instance_path):
-    #domain  = read_domain(domain_path)
-    domain  = read_domain("Input/NewPlansformer/driverlog/domain/domain.pddl")
-    print(f"Domain is: {domain_path} and read domain is: {str(domain)}")
+    domain  = read_domain(domain_path)
     plan = read_plan(plan_text)
     goal = read_goal(instance_path)
     init_cond = read_init(instance_path)
