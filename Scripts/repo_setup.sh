ac51da2fec6b9cf34a6b30ace08fefa0f42561e4
fabiano <francesco.fabiano@unipr.it>
2023-07-13 15:39:03 +0100

    Finalized repo managing

--- Scripts/repo_setup.sh
+++ Scripts/repo_setup.sh
@@ -21,25 +21,26 @@ echo -e "\nDONE Preparing FastDownward\n\n\n"
 
 
 ## Plansformers Management
-#pip install --upgrade --no-cache-dir gdown
+pip install --upgrade --no-cache-dir gdown
 
-#echo -e "\n\n\nPreparing Plansformer v1.0\n"
-#cd Planners/PlansformerS1/model_files
+echo -e "\n\n\nPreparing Plansformer v1.0\n"
+cd Planners/PlansformerS1/model_files
 #Plansformer 1.0
-#gdown "https://drive.google.com/uc?id=1960ZL8-iYBvV3zwYqF1zL8CMhT8ugFPq"
-#cd ../../..
-#echo -e "\nDONE Preparing Plansformer v1.0\n\n\n"
+gdown "https://drive.google.com/uc?id=1960ZL8-iYBvV3zwYqF1zL8CMhT8ugFPq"
+cd ../../..
+echo -e "\nDONE Preparing Plansformer v1.0\n\n\n"
 
-#sleep 5  # Waits 5 seconds.
+sleep 5  # Waits 5 seconds.
 
-#echo -e "\n\n\nPreparing Plansformer v2.0\n"
-#cd Planners/New_PlansformerS1/mode0_model_files
+echo -e "\n\n\nPreparing Plansformer v2.0\n"
+cd Planners/New_PlansformerS1/mode0_model_files
 ##Plansformer 2.0 -- MODE 0 File
 #gdown "https://drive.google.com/uc?id=1960ZL8-iYBvV3zwYqF1zL8CMhT8ugFPq"
-#cd ../mode1_model_files
 #sleep 5  # Waits 5 seconds.
-##Plansformer 2.0 -- MODE 1 File
-#gdown "https://drive.google.com/uc?id=1d8csJA9tGRNfj3ugktuhtucq4c9vSdPg"
-#cd ..
-#mkdir mode2_model_files #Plansformer 2.0 -- MODE 2 Folder
-#echo -e "\nDONE Preparing Plansformer v2.0\n\n\n"
+
+cd ../mode1_model_files
+#Plansformer 2.0 -- MODE 1 File
+gdown "https://drive.google.com/uc?id=1d8csJA9tGRNfj3ugktuhtucq4c9vSdPg"
+cd ..
+mkdir mode2_model_files #Plansformer 2.0 -- MODE 2 Folder
+echo -e "\nDONE Preparing Plansformer v2.0\n\n\n"
