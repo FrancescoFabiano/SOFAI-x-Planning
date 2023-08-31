0783d8e35d77316d7a40ac98d805e0e6ae67171a
FrancescoFabiano <francesco.fabi94@gmail.com>
2023-08-31 14:57:43 -0600

    Changed LPG solving from speed to quality

--- Scripts/LPG_solve.sh
+++ Scripts/LPG_solve.sh
@@ -17,7 +17,9 @@ if [ -f "$OUTTMPFILE.SOL" ] ; then
     rm "$OUTTMPFILE.SOL"
 fi;
 
-timeout "$TIMEOUT" ./Planners/LPG-td-1.4/lpg-td -o $DOMAINFOLDER$DOMAIN -f $PROBLEMFOLDER$PROBLEM -speed -out $OUTTMPFILE > res.tmp
+#timeout "$TIMEOUT" ./Planners/LPG-td-1.4/lpg-td -o $DOMAINFOLDER$DOMAIN -f $PROBLEMFOLDER$PROBLEM -speed -out $OUTTMPFILE > res.tmp
+timeout "$TIMEOUT" ./Planners/LPG-td-1.4/lpg-td -o $DOMAINFOLDER$DOMAIN -f $PROBLEMFOLDER$PROBLEM -quality -out $OUTTMPFILE > res.tmp
+
 
 if [ $? -eq 124 ]; then
   echo -e "LPG TIMED-OUT" >> "$OUTTMPFILE.SOL"
