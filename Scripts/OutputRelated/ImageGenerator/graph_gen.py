50ab30e0ea21432ff69ad1049a7f64cc61b8e78a
FrancescoFabiano <fabianofrancesco.cs@gmail.com>
2023-09-03 17:19:27 -0600

    Automated latex table printing

--- Scripts/OutputRelated/ImageGenerator/graph_gen.py
+++ Scripts/OutputRelated/ImageGenerator/graph_gen.py
@@ -97,6 +97,16 @@ def sol_reader(filename,rootFilename,suffix):
     df.sort_values(sort_order, ascending=True,inplace=True,na_position='first')
     df.to_csv(modfilename, index=False)
 
+def loopPrintLaTeX(line,narg,tableFile):
+    for i in range(narg):
+        print(line,file=tableFile,end="")
+
+def loopPrintLaTeXArray(line_before,endline,endSequence,array,tableFile):
+    for elem in array[:-1]:
+        print(line_before + str(elem),file=tableFile,end=endline)
+    print(line_before + str(array[-1]),file=tableFile,end=endSequence)
+    
+
 if __name__ == '__main__':
 
     plotting_val = (sys.argv[1])
@@ -128,8 +138,10 @@ if __name__ == '__main__':
 
     merged_df = functools.reduce(lambda left, right: pd.merge(left,right,on='Name'), dataFrames)
 
-
-    merged_name = "Input/Merged/merged.csv"
+    merged_path="Output/Merged/"
+    Path(merged_path).mkdir(parents=True,exist_ok=True)
+    
+    merged_name = merged_path+"merged.csv"
     merged_df.to_csv(merged_name, encoding='utf-8', index=False)
 
 
@@ -181,19 +193,19 @@ if __name__ == '__main__':
 
 
 
-    nInstances = len(df)+1
+    nRows = len(df)+1
     countCases = 0
     col_numbers = 5
-    merged_name_formula = "Input/Merged/merged_formulae.csv"
+    merged_name_formula = merged_path+"merged_formulae.csv"
     merged_df.to_csv(merged_name_formula, encoding='utf-8', index=False)
 
-    formula_row = "\"=SUBTOTAL(3,"+chr(ord('A') + 0)+"2:"+chr(ord('A') + 0)+str(nInstances) +")\","
+    formula_row = "\"=SUBTOTAL(3,"+chr(ord('A') + 0)+"2:"+chr(ord('A') + 0)+str(nRows) +")\","
     while countCases < narg:
         increaser = countCases*col_numbers
-        formula_row += "\"=SUBTOTAL(1,"+chr(ord('B') + increaser)+"2:"+chr(ord('B') + increaser)+str(nInstances) +")\","
-        formula_row += "\"=SUBTOTAL(1,"+chr(ord('C') + increaser)+"2:"+chr(ord('C') + increaser)+str(nInstances) +")\","
-        formula_row += "\"=SUBTOTAL(1,"+chr(ord('D') + increaser)+"2:"+chr(ord('D') + increaser)+str(nInstances) +")\","
-        formula_row += "\"=COUNTIF("+chr(ord('E') + increaser)+"2:"+chr(ord('E') + increaser)+str(nInstances) +",1)\""
+        formula_row += "\"=SUBTOTAL(1,"+chr(ord('B') + increaser)+"2:"+chr(ord('B') + increaser)+str(nRows) +")\","
+        formula_row += "\"=SUBTOTAL(1,"+chr(ord('C') + increaser)+"2:"+chr(ord('C') + increaser)+str(nRows) +")\","
+        formula_row += "\"=SUBTOTAL(1,"+chr(ord('D') + increaser)+"2:"+chr(ord('D') + increaser)+str(nRows) +")\","
+        formula_row += "\"=COUNTIF("+chr(ord('E') + increaser)+"2:"+chr(ord('E') + increaser)+str(nRows) +",1)\""
         formula_row += "," #For Empty column -- Type of system 2 does not have an aggregate
         if countCases < narg:
             formula_row += ","
@@ -204,3 +216,287 @@ if __name__ == '__main__':
     f = open(merged_name_formula, "a")
     f.write(formula_row)
     f.close()
+
+    #Create LaTeX Table
+    table_path="Output/Table/"
+    Path(table_path).mkdir(parents=True,exist_ok=True)
+
+    with open(table_path+"table.tex", "w") as tableFile:
+        #Preamble
+        print('\\documentclass[12pt,a4paper]{standalone}\n\\usepackage{hhline}\n\\usepackage{tabularx}\n\\begin{document}',file=tableFile)
+        
+        #Tabular declaration
+        print('\t\\begin{tabular}{||l',file=tableFile,end="")
+        loopPrintLaTeX('|c',narg,tableFile)
+        print('||}',file=tableFile)
+
+        #Approaches row
+        print('\t\t\\hhline{~|t:',file=tableFile,end="")
+        loopPrintLaTeX('=',narg,tableFile)
+        print(':t|}',file=tableFile)
+        print("\t\t\\multicolumn{1}{c||}{} & \multicolumn{1}{c|}", file=tableFile,end="")
+        loopPrintLaTeXArray("{\\texttt{","}} & ","}}\\\\\n",suffixes,tableFile)
+        print('\t\t\\hhline{|t:=:b:',file=tableFile,end="")
+        loopPrintLaTeX('=',narg,tableFile)
+        print(':t}',file=tableFile)
+
+        #Number of plans Section
+
+        solved_plans_stats = []
+        correct_plans_stats = []
+        partial_plans_stats = []
+        partial_plansArr = []
+        max_solved_plans = -1
+        index_max = []
+        count_plans = 0
+        nInstances = nRows-1
+        dictValues = {}
+        for suff in suffixes:
+            
+            dictValues.clear()
+            dictValues = merged_df[f'Corr-{suff}'].value_counts().to_dict()
+
+            if 0.0 in dictValues.keys():
+                failed_plans = dictValues[0.0]
+            else:
+                failed_plans = 0
+
+            if 1.0 in dictValues.keys():
+                correct_plans = dictValues[1.0]
+            else:
+                correct_plans = 0
+
+
+            solved_plans = nInstances - failed_plans
+            partial_plans = solved_plans - correct_plans
+
+            if solved_plans > max_solved_plans:
+                max_solved_plans = solved_plans
+                index_max.clear()
+                index_max.append(count_plans)
+            elif solved_plans == max_solved_plans:
+                index_max.append(count_plans)
+
+            solved_plans_stats.append(f"{solved_plans} ({(solved_plans / nInstances)*100.0:.2f}\%)")
+            correct_plans_stats.append(f"{correct_plans} ({(correct_plans / nInstances)*100.0:.2f}\%)")
+            partial_plans_stats.append(f"{partial_plans} ({(partial_plans / nInstances)*100.0:.2f}\%)")
+            partial_plansArr.append(partial_plans)
+
+            count_plans += 1
+
+        for c in index_max:
+            solved_plans_stats[c] = "\\textbf{"+solved_plans_stats[c]+"}"
+
+        print("\t\t%Number of Plans", file=tableFile)
+        print("\t\tPlans & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",solved_plans_stats,tableFile)
+
+
+        print('\t\t\\hhline{||',file=tableFile,end="")
+        loopPrintLaTeX('-',narg+1,tableFile)
+        print('||}',file=tableFile)
+
+        print("\t\t\\quad Complete & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",correct_plans_stats,tableFile)
+
+        print('\t\t\\hhline{||',file=tableFile,end="")
+        loopPrintLaTeX('-',narg+1,tableFile)
+        print('||}',file=tableFile)
+
+        print("\t\t\\quad Partial & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",partial_plans_stats,tableFile)
+
+
+        print('\t\t\\hhline{|:',file=tableFile,end="")
+        loopPrintLaTeX('=',narg+1,tableFile)
+        print(':|}',file=tableFile)
+
+        #S1 Plans
+        sys1PlansArr = []
+        sys1PlanClean = []
+        sys2PlansArr = []
+
+        for suff in suffixes:
+            dictValues = merged_df[f'Sys-{suff}'].value_counts().to_dict()
+
+           
+            if -1 in dictValues.keys():
+                failedPlans = dictValues[-1]
+            else:
+                failedPlans = 0
+
+            if 1 in dictValues.keys():
+                sys1Plans = dictValues[1]
+            else:
+                sys1Plans = 0
+
+            if 2 in dictValues.keys():
+                sys2Plans = dictValues[2]
+            else:
+                sys1Plans = 0
+
+            sys1PlansArr.append(f"{sys1Plans} ({(sys1Plans / nInstances)*100.0:.2f}\%)")
+            sys1PlanClean.append(sys1Plans)
+            sys2PlansArr.append(f"{sys2Plans} ({(sys2Plans / nInstances)*100.0:.2f}\%)")
+
+        print("\t\t\\texttt{S1} Plans & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",sys1PlansArr,tableFile)
+
+        print('\t\t\\hhline{||',file=tableFile,end="")
+        loopPrintLaTeX('-',narg+1,tableFile)
+        print('||}',file=tableFile)
+
+
+
+        sys1_completePlans = []
+        c = 0
+        while c in range(len(partial_plansArr)):
+            sys1_completePlans.append(int(sys1PlanClean[c])-int(partial_plansArr[c]))
+            c+=1
+
+        print("\t\t\\quad Complete & ", file=tableFile,end="")
+        
+        sys1_complete_stats = []
+        for elem in sys1_completePlans:
+            sys1_complete_stats.append(f"{elem} ({(elem / nInstances)*100.0:.2f}\%)")
+        loopPrintLaTeXArray(""," & ","\\\\\n",sys1_complete_stats,tableFile)
+
+        print('\t\t\\hhline{||',file=tableFile,end="")
+        loopPrintLaTeX('-',narg+1,tableFile)
+        print('||}',file=tableFile)
+
+        print("\t\t\\quad Partial & ", file=tableFile,end="")
+        sys1_partial_stats = []
+        for elem in partial_plansArr:
+            sys1_partial_stats.append(f"{elem} ({(elem / nInstances)*100.0:.2f}\%)")
+        loopPrintLaTeXArray(""," & ","\\\\\n",sys1_partial_stats,tableFile)
+
+
+        print('\t\t\\hhline{|:',file=tableFile,end="")
+        loopPrintLaTeX('=',narg+1,tableFile)
+        print(':|}',file=tableFile)
+
+
+        print("\t\t\\texttt{S2} Plans & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",sys2PlansArr,tableFile)
+
+
+
+        FD_uses = []
+        LPG_uses = []
+        FDLPG_uses = []
+        dictValues.clear()
+        for suff in suffixes:
+    
+            df_Sys2_cleaned = merged_df
+            df_Sys2_cleaned = df_Sys2_cleaned[ (df_Sys2_cleaned[f'Sys-{suff}'] == 2) ]
+            dictValues = df_Sys2_cleaned[f'Planner-{suff}'].value_counts().to_dict()
+
+            if 1 in dictValues.keys():
+                fdPlans = dictValues[1]
+            else:
+                fdPlans = 0
+
+            if 2 in dictValues.keys():
+                lpgPlans = dictValues[2]
+            else:
+                lpgPlans = 0
+
+            if 3 in dictValues.keys():
+                pfLpgPlans = dictValues[3]
+            else:
+                pfLpgPlans = 0
+
+            FD_uses.append(f"{fdPlans} ({(fdPlans / nInstances)*100.0:.2f}\%)")
+            LPG_uses.append(f"{lpgPlans} ({(lpgPlans / nInstances)*100.0:.2f}\%)")
+            FDLPG_uses.append(f"{pfLpgPlans} ({(pfLpgPlans / nInstances)*100.0:.2f}\%)")
+
+        print('\t\t\\hhline{||',file=tableFile,end="")
+        loopPrintLaTeX('-',narg+1,tableFile)
+        print('||}',file=tableFile)
+
+        print("\t\t\\quad FD & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",FD_uses,tableFile)
+
+        print('\t\t\\hhline{||',file=tableFile,end="")
+        loopPrintLaTeX('-',narg+1,tableFile)
+        print('||}',file=tableFile)
+
+        print("\t\t\\quad LPG & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",LPG_uses,tableFile)
+
+        print('\t\t\\hhline{||',file=tableFile,end="")
+        loopPrintLaTeX('-',narg+1,tableFile)
+        print('||}',file=tableFile)
+
+        print("\t\t\\quad PF + LPG & ", file=tableFile,end="")
+        loopPrintLaTeXArray(""," & ","\\\\\n",FDLPG_uses,tableFile)
+
+        print('\t\t\\hhline{|:',file=tableFile,end="")
+        loopPrintLaTeX('=',narg+1,tableFile)
+        print(':|}',file=tableFile)
+
+
+
+        print("\t\tTime (avg) & ", file=tableFile,end="")
+        timeArr = []
+        minTime = sys.maxsize
+        index_min = []
+        count_time = 0
+        for suff in suffixes:
+            timeAvg = merged_df[f'Time-{suff}'].mean()/1000
+
+
+            if timeAvg < minTime:
+                minTime = timeAvg
+                index_min.clear()
+                index_min.append(count_time)
+            elif minTime == max_solved_plans:
+                index_max.append(count_time)
+            count_time +=1
+
+            timeArr.append(f"{timeAvg:.3f}s")
+
+        for c in index_min:
+            timeArr[c] = "\\textbf{"+timeArr[c]+"}"
+        loopPrintLaTeXArray(""," & ","\\\\\n",timeArr,tableFile)
+
+        print('\t\t\\hhline{|:',file=tableFile,end="")
+        loopPrintLaTeX('=',narg+1,tableFile)
+        print(':|}',file=tableFile)
+
+
+        print("\t\tOptimality (avg) & ", file=tableFile,end="")
+       
+        df_cleaned = merged_df
+        for suff in suffixes:
+            df_cleaned = df_cleaned[ (df_cleaned[f'Opt-{suff}'] >= 0) ]
+        
+        optArr = []
+        minOpt = sys.maxsize
+        index_min = []
+        count_opt = 0
+        for suff in suffixes:
+            optAvg = df_cleaned[f'Opt-{suff}'].mean()
+
+
+            if optAvg < minOpt:
+                minOpt = optAvg
+                index_min.clear()
+                index_min.append(count_opt)
+            elif minOpt == max_solved_plans:
+                index_max.append(count_opt)
+            count_opt +=1
+
+            optArr.append(f"+{optAvg:.2f}\%")
+
+        for c in index_min:
+            optArr[c] = "\\textbf{"+optArr[c]+"}"
+        loopPrintLaTeXArray(""," & ","\\\\\n",optArr,tableFile)
+
+        print('\t\t\\hhline{|b:',file=tableFile,end="")
+        loopPrintLaTeX('=',narg+1,tableFile)
+        print(':b|}',file=tableFile)
+
+
+        print('\t\\end{tabular}\n\\end{document}',file=tableFile)
