d10206c1284bfa2b8510c2182982414b55adf1c2
FrancescoFabiano <fabianofrancesco.cs@gmail.com>
2023-09-02 20:48:27 -0600

    optimality checker

--- Scripts/ProblemGenerator/completeGenerator.sh
+++ Scripts/ProblemGenerator/completeGenerator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Script to generate $1 problem files for the domains. These instances should require at least $2 seconds to be solved by the fastet planner we have and within $3 tries
-# Usage from the "Scripts/ProblemGenerator" folder execute "./completeGenerator.sh 20 0.5 1000" 20 instances for all domains.
-# Each instance will be below 512 token and should require more than 0.5 seconds to be solved by LPG. Finally, all domains will try to generate a maximum of 1000 instances
+# Script to generate $1 problem files for the domains. These instances should require at least $2 seconds to be solved by the fastet planner we have and within $3 tries. $4 Activate (if == 1) optimality calculation
+# Usage from the "Scripts/ProblemGenerator" folder execute "./completeGenerator.sh 20 0.5 1000 1"
+# Each instance will be below 512 token and should require more than 0.5 seconds to be solved by LPG. Finally, all domains will try to generate a maximum of 1000 instances and for each one the optiaml plan is calculated
 
 max_tokens=512
 outFolder="Output/"
@@ -10,8 +10,11 @@ instFolderEasy="instancesEasy/"
 instFolderHard="instancesHard/"
 instFolderBigEasy="instancesBigEasy/"
 instFolderBigHard="instancesBigHard/"
+origOptPath="noOpt/"
 minimumTime=$2
 declare -i maxAttemptsPerDomain=$3
+optimality=$4
+optimality_time=300
 
 #Blocksworld specific
 declare -i maxBlockNum=16
@@ -35,8 +38,9 @@ hanoiFolder="hanoi/"
 miconicFolder="miconic/"
 
 ## declare an array variable
-#declare -a dmnArr=("$bwFolder" "$dlFolder" "$ferryFolder" "$grFolder" "$hanoiFolder" "$miconicFolder")
-declare -a dmnArr=("$hanoiFolder" "$miconicFolder" "$grFolder" "$ferryFolder" "$dlFolder" "$bwFolder" )
+#declare -a dmnArr=("$bwFolder")
+#declare -a dmnArr=("$hanoiFolder" "$miconicFolder" "$grFolder" "$ferryFolder" "$dlFolder" "$bwFolder" )
+declare -a dmnArr=("$bwFolder")
 
 ## now loop through the above array
 for domain in "${dmnArr[@]}"
@@ -59,10 +63,10 @@ do
 
 
   mkdir -p $tempDmnFolder
-  mkdir -p $tempInstancesDirEasy
-  mkdir -p $tempInstancesDirHard
-  mkdir -p $tempInstancesDirBigEasy
-  mkdir -p $tempInstancesDirBigHard
+  mkdir -p $tempInstancesDirEasy$origOptPath
+  mkdir -p $tempInstancesDirHard$origOptPath
+  mkdir -p $tempInstancesDirBigEasy$origOptPath
+  mkdir -p $tempInstancesDirBigHard$origOptPath
 
 
   declare -i attemptPerDomain=0
@@ -94,10 +98,24 @@ do
     if [ -z "$(ls -A $tempDmnFolder)" ]; then
       cp $domain_file $tempDmnFolder/domain.pddl
     fi
-    
+
+    #Optimality checker
+    if (( $optimality == 1)); then
+      timeout "$optimality_time"s python3 ../../Planners/FastDownward/fast-downward.py --plan-file planopt.tmp $domain_file $problem_file  --search "astar(blind())" > /dev/null
+      if [ $? -eq 124 ]; then #If we take more than the minimum time to solve with LPG fast we add it
+        optimal_lenght=-1
+        optPath=$origOptPath
+      else
+        optimal_lenght=`wc --lines < planopt.tmp`
+        optimal_lenght=$((optimal_lenght - 1))
+        optPath=""
+      fi
+    fi
+    echo -e "; Optimality lenght is </optlen>$optimal_lenght</>\n" | cat - $problem_file > optFileName.tmp && mv optFileName.tmp $problem_file
+
+    #Checking for token limitations
     prompt=$(python plansformer_prompt_generator.py "$domain_file" "$problem_file")
     token_count=$(python count_512subset.py "$prompt")
-
     #token_count=100
     
     filename=$(basename -- "$problem_file")
@@ -107,20 +125,24 @@ do
       #Checking for solving time limitations
       timeout "$minimumTime"s ./../../Planners/LPG-td-1.4/lpg-td -o $domain_file -f $problem_file -speed -out plan.tmp > /dev/null
       if [ $? -eq 124 ]; then #If we take more than the minimum time to solve with LPG fast we add it
-        cp $problem_file $tempInstancesDirHard/$filename
-        counter=$((counter + 1))
+        cp $problem_file $tempInstancesDirHard$optPath$filename
+        
+        if (( $optimal_lenght > 0)); then
+          counter=$((counter + 1))
+        fi
+        
         echo "Added problem" $filename
       else
-        cp $problem_file $tempInstancesDirEasy/$filename
+        cp $problem_file $tempInstancesDirEasy$optPath$filename
         echo "Discarded problem" $filename  "solved too fast by LPG"
       fi
     else
       echo "Discarded problem" $filename  "because has too many tokens"
       timeout "$minimumTime"s ./../../Planners/LPG-td-1.4/lpg-td -o $domain_file -f $problem_file -speed > /dev/null
       if [ $? -eq 124 ]; then #If we take more than the minimum time to solve with LPG fast we add it
-        cp $problem_file $tempInstancesDirBigHard/$filename
+        cp $problem_file $tempInstancesDirBigHard$optPath$filename
       else
-        cp $problem_file $tempInstancesDirBigEasy/$filename
+        cp $problem_file $tempInstancesDirBigEasy$optPath$filename
       fi      
     fi 
 
