e24c21561be06a593d450f26c732bbc79b0a8b28
FrancescoFabiano <francesco.fabi94@gmail.com>
2023-11-30 11:45:48 -0700

    Results of Execution

--- Scripts/batch_list_all_config_test.sh
+++ Scripts/batch_list_all_config_test.sh
@@ -16,7 +16,7 @@ system2_configs=("0" "1" "2" "3" "4")
 system2_names=("" "FD" "LPG" "LPGxLPG" "FDxLPG")
 
 #used_S1configs=("2" "4" "6")
-used_S1configs=("0" "7")
+used_S1configs=("7" "0")
 #used_S2configs=("1" "2" "3" "4")
 used_S2configs=("0" "1" "2" "3" "4")
 
